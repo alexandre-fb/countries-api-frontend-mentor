4e456b9d9594d3238e64e7033a5e1d88c6b48823
Alexandre Bisognin <alexandrefb.86@gmail.com>
2022-06-27 20:54:38 -0300

    Refactor

--- src/components/main-home/styles.jsx
+++ src/components/main-home/styles.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
-export const Container = styled.main``;
+export const Container = styled.main`
+`;
 
 export const Countries = styled.ul`
   display: grid;
