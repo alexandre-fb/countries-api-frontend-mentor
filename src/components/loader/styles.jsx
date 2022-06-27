b34f92befd50dc4be712cf86ff8813b2915e06a3
Alexandre Bisognin <alexandrefb.86@gmail.com>
2022-06-27 17:51:32 -0300

    Refactor

---
+++ src/components/loader/styles.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  padding: 150px 0;
+
+  div {
+    width: 100px;
+    height: 100px;
+  }
+`;
