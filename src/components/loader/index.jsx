9ad8c93a7a736adb56798bf92cdd81adad4ed50b
Alexandre Bisognin <alexandrefb.86@gmail.com>
2022-06-27 15:24:16 -0300

    Update Loader component

--- src/components/loader/index.jsx
+++ src/components/loader/index.jsx
@@ -1,16 +1,28 @@
 import Lottie from "lottie-react";
 import styled from "styled-components";
-import spinningGlobe from "../../assets/lottie-animations/spinning-globe.json";
+import globe from "../../assets/lottie-animations/earth-globe.json";
 
 export const Loader = () => {
   return (
     <Container>
-      <Lottie animationData={spinningGlobe} autoplay={true} loop={false} />
+      <div>
+        <Lottie animationData={globe} autoplay={true} loop={true} />
+        <p>Carregando...</p>
+      </div>
     </Container>
   );
 };
 
 const Container = styled.div`
-  width: 100px;
-  height: 100px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  padding: 150px 0;
+
+  div {
+    width: 100px;
+    height: 100px;
+  }
 `;
