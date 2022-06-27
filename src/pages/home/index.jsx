92bcc0e0b0097115db4f0844164f8e15c2850ba7
Alexandre Bisognin <alexandrefb.86@gmail.com>
2022-06-27 20:56:38 -0300

    Create the ContainerPage component and insert Footer in Home Page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,15 +1,14 @@
 import styled from "styled-components";
+import { ContainerPage } from "../../components/ContainerPages";
 import { Header } from "../../components/header";
 import { MainHome } from "../../components/main-home";
 import { Footer } from "../../components/footer";
 export const Home = () => {
   return (
-    <Container>
+    <ContainerPage>
       <Header />
       <MainHome />
       <Footer />
-    </Container>
+    </ContainerPage>
   );
 };
-
-const Container = styled.div``;
